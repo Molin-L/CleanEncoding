f886b0b92a79d54458d102626d4f0abae232804a
Molin Liu <molin@live.cn>
2021-04-23 17:28:10 +0800

    Initialization

---
+++ processing.cpp
@@ -0,0 +1,19 @@
+// Created on Molin’s ipad pro.
+
+#include <iostream>
+#include <pthread.h>
+#inlcude <Python.h>
+
+enum TRANS_ERRORS{
+    FILE_NOTFOUND,
+    FILE_PRIVELEGE,
+    ENCODING_ERROR
+}
+
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+    return 0;
+}
